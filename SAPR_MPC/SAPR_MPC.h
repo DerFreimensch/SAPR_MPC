3f81589ada4ad9eda153527f7291bfa923d6ec5b
DerFreimensch <kostrm030102@gmail.com>
2021-05-31 14:45:50 +0300

    Добавьте файлы проекта.

---
+++ SAPR_MPC/SAPR_MPC.h
@@ -0,0 +1,32 @@
+﻿
+// SAPR_MPC.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CSAPR_MPCApp:
+// Сведения о реализации этого класса: SAPR_MPC.cpp
+//
+
+class CSAPR_MPCApp : public CWinApp
+{
+public:
+	CSAPR_MPCApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSAPR_MPCApp theApp;
